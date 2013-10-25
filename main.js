e5f23542bf389a91a258bb914c8ab440395ffc8f
fuqcool <fuqcool@gmail.com>
2013-10-25 14:42:56 -0400

    accept file name as arguments

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 var http = require('http');
 var socket = require('socket.io');
 var fs = require('fs');
+var path = require('path');
 var shell = require('shelljs');
+var program = require('commander');
+
+program.parse(process.argv);
+
+var filePath = null;
+
+if (program.args.length) {
+  filePath = program.args[0];
+  fs.exists(filePath, function (exists) {
+    if (!exists) {
+      console.log(filePath + ' does not exists');
+      process.exit();
+    }
+  });
+} else {
+  console.log('invalid options');
+  process.exit();
+}
 
 var server = http.createServer(handler);
 var io = socket.listen(server);
 server.listen(5277);
 
-var FILE_NAME = 'README';
 var VIEWER = 'viewer.html';
 
 function handler(req, res) {
-  fs.readFile(__dirname + '/' + VIEWER, function (err, data) {
+  fs.readFile(path.join(__dirname, VIEWER), function (err, data) {
+    if (err) {
+      console.log('failed to open viewer');
+      process.exit();
+    }
     res.writeHead(200);
     res.end(data);
   });
 }
 
 io.sockets.on('connection', function (socket) {
-  var watcher = fs.watch(FILE_NAME, function (event, filename) {
+  var watcher = fs.watch(filePath, function (event, filename) {
     if (event === 'change') {
       console.log(filename + ' updated');
       var page = shell.exec('kramdown README').output;
@@ -29,6 +51,7 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
     console.log('disconnected');
+    watcher.close();
     process.exit();
   });
 });
