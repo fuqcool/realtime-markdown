04fe6878ea73852c0c073a07c610cbd50808e0e7
fuqcool <fuqcool@gmail.com>
2013-10-25 13:48:12 -0400

    process exit on disconnect

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var server = http.createServer(handler);
 var io = socket.listen(server);
 server.listen(5277);
 
-var FILE_NAME = 'test.md';
+var FILE_NAME = 'README';
 var VIEWER = 'viewer.html';
 
 function handler(req, res) {
@@ -18,12 +18,17 @@ function handler(req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
-  fs.watch(FILE_NAME, function (event, filename) {
+  var watcher = fs.watch(FILE_NAME, function (event, filename) {
     if (event === 'change') {
       console.log(filename + ' updated');
-      var page = shell.exec('kramdown test.md').output;
+      var page = shell.exec('kramdown README').output;
 
       socket.emit('update', page);
     }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('disconnected');
+    process.exit();
   });
 });
